8439fb6908814d2d106b5cc867afcfb2d56c2cf6
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-01-26 14:21:21 -0800

    Prepare for cross-Launchpad-model compatibility

--- midiout.cpp
+++ midiout.cpp
@@ -27,24 +27,20 @@
 
 #ifdef _WIN32
 	//define something for Windows (32-bit and 64-bit, this part is common)
-	#define LAUNCHPAD_IN "MIDIIN2 (Launchpad Pro)"
-	#define LAUNCHPAD_OUT "MIDIOUT2 (Launchpad Pro)"
 
 	#ifdef _WIN64
 		//define something for Windows (64-bit only)
 	#endif
-#elif __APPLE__
-	//define something for Mac
-	#define LAUNCHPAD_IN "Launchpad Pro Standalone Port"
-	#define LAUNCHPAD_OUT "Launchpad Pro Standalone Port"
-#else
-	#error "Unknown/unsupported compiler/operating system"
+#else//#elif __APPLE__
+	//define something for Mac (and Linux(?))
+//#else
+	//#error "Unknown/unsupported compiler/operating system"
 #endif
 
 #define ABLETON_LIVE_MODE 4
 #define MIDI_MESSAGE_SYSEX_BEGIN 240
 #define MIDI_MESSAGE_SYSEX_END 247
-#define LAUNCHPAD_SIDE_LED_ID 99
+#define LAUNCHPAD_PRO_SIDE_LED_ID 99
 
 int BPM = 120;
 
@@ -75,6 +71,8 @@ class Launchpad {
 class LaunchpadPro : public Launchpad {
 	public:
 		LaunchpadPro();
+		int connect();
+		void disconnect();
 		void setColor(unsigned char msg_type, unsigned char light, unsigned char color);
 		void setColor(unsigned char msg_type, unsigned char light, unsigned char red, unsigned char green, unsigned char blue);
 		void setFlash(unsigned char msg_type, unsigned char light, unsigned char color);
@@ -85,10 +83,11 @@ class LaunchpadPro : public Launchpad {
 class LaunchpadS : public Launchpad {
 	public:
 		LaunchpadS();
+		int connect();
+		void disconnect();
 		void setColor(unsigned char msg_type, unsigned char light, unsigned char color);
 		void setPulse(unsigned char msg_type, unsigned char light, unsigned char color);
-	private:
-		unsigned char pro_to_s_note(unsigned char pro_note);
+		unsigned char pro_to_s_note(unsigned char pro_note, unsigned char msg_type);
 		unsigned char pro_to_s_color(unsigned char pro_color);
 };
 
@@ -115,28 +114,11 @@ int Launchpad::connect() {
 	// Don't ignore sysex, timing, or active sensing messages.
 	midiin->ignoreTypes( false, false, false );
 
-	std::vector<unsigned char> device_info;
-
-	// Everything below is for the Launchpad Pro
-
-	// Inquiry Device
-	sendMessage( 240, 126, 127, 6, 1, 247 );
-	while (device_info.size() == 0) {
-		getMessage(&device_info);
-		usleep( 10000 ); // Sleep for 10 milliseconds ... platform-dependent.
-	}
-
-	sendMessage( 240, 0, 32, 41, 2, 16, 33, 1, 247 ); // Set to Standalone Mode
-	sendMessage( 240, 0, 32, 41, 2, 16, 44, 3, 247 ); // Set to Programmer Mode
-
 	return 0;
+
 }
 
 void Launchpad::disconnect() {
-	// Two below are Launchpad Pro
-	sendMessage( 240, 0, 32, 41, 2, 16, 14, 0, 247 ); // Clear all LED colors
-	sendMessage( 240, 0, 32, 41, 2, 16, 44, 0, 247 ); // Set to Note Mode
-
 	delete midiin;
 	delete midiout;
 }
@@ -165,11 +147,11 @@ void Launchpad::sendMessage(unsigned int first_byte, ...) {
 	va_list vl;
 	va_start(vl, first_byte);
 	unsigned char byte = first_byte;
-	while (byte != MIDI_MESSAGE_SYSEX_END || byte > 255) {
+	while (byte != MIDI_MESSAGE_SYSEX_END || byte < 0 || byte > 255) {
 		message.push_back(byte);
 		byte = va_arg(vl, unsigned int);
 	}
-	message.push_back(byte);
+	if (byte >= 0 && byte <= 255) message.push_back(byte);
 	va_end(vl);
 
 	midiout->sendMessage(&message);
@@ -185,8 +167,39 @@ void Launchpad::setPulse(unsigned char msg_type, unsigned char light, unsigned c
 }
 
 LaunchpadPro::LaunchpadPro() {
-	INPORT_NAME = "Launchpad Pro Standalone Port";
-	OUTPORT_NAME = "Launchpad Pro Standalone Port";
+	#ifdef _WIN32
+		INPORT_NAME = "MIDIIN2 (Launchpad Pro)";
+		OUTPORT_NAME = "MIDIOUT2 (Launchpad Pro)";
+	#else
+		INPORT_NAME = "Launchpad Pro Standalone Port";
+		OUTPORT_NAME = "Launchpad Pro Standalone Port";
+	#endif
+}
+
+int LaunchpadPro::connect() {
+	int x = Launchpad::connect();
+
+	if (x == 0) {
+		std::vector<unsigned char> device_info;
+
+		// Inquiry Device
+		sendMessage( 240, 126, 127, 6, 1, 247 );
+		while (device_info.size() == 0) {
+			getMessage(&device_info);
+			usleep( 10000 ); // Sleep for 10 milliseconds ... platform-dependent.
+		}
+
+		sendMessage( 240, 0, 32, 41, 2, 16, 33, 1, 247 ); // Set to Standalone Mode
+		sendMessage( 240, 0, 32, 41, 2, 16, 44, 3, 247 ); // Set to Programmer Mode
+	}
+	return x;
+}
+
+void LaunchpadPro::disconnect() {
+	sendMessage( 240, 0, 32, 41, 2, 16, 14, 0, 247 ); // Clear all LED colors
+	sendMessage( 240, 0, 32, 41, 2, 16, 44, 0, 247 ); // Set to Note Mode
+
+	Launchpad::disconnect();
 }
 
 void LaunchpadPro::setColor(unsigned char msg_type, unsigned char light, unsigned char color) {
@@ -216,22 +229,52 @@ void LaunchpadPro::displayText(unsigned char msg_type, unsigned int color, unsig
 }
 
 LaunchpadS::LaunchpadS() {
-	INPORT_NAME = "Launchpad S";
-	OUTPORT_NAME = "Launchpad S";
+	#ifdef _WIN32 // May not be accurate!
+		INPORT_NAME = "Launchpad S";
+		OUTPORT_NAME = "Launchpad S";
+	#else
+		INPORT_NAME = "Launchpad S";
+		OUTPORT_NAME = "Launchpad S";
+	#endif
 }
 
-unsigned char pro_to_s_note(unsigned char pro_note) {
+int LaunchpadS::connect() {
+	int x = Launchpad::connect();
+
+	if (x == 0) {
+		std::vector<unsigned char> device_info;
+
+		// Inquiry Device
+		sendMessage( 240, 126, 127, 6, 1, 247 );
+		while (device_info.size() == 0) {
+			getMessage(&device_info);
+			usleep( 10000 ); // Sleep for 10 milliseconds ... platform-dependent.
+		}
+
+		sendMessage( 176, 0, 2, -1 ); // Set to Drum Rack Mode
+	}
+	return x;
+}
+
+void LaunchpadS::disconnect() {
+	sendMessage( 176, 0, 0, -1 ); // Reset
+
+	Launchpad::disconnect();
+}
+
+unsigned char LaunchpadS::pro_to_s_note(unsigned char pro_note, unsigned char msg_type) {
+	if (msg_type == 176) return pro_note;
 	return pro_note;
 }
 
-unsigned char pro_to_s_color(unsigned char pro_color) {
+unsigned char LaunchpadS::pro_to_s_color(unsigned char pro_color) {
 	return pro_color;
 }
 
 void LaunchpadS::setColor(unsigned char msg_type, unsigned char light, unsigned char color) {
 	message.push_back(144);
-	message.push_back(/*pro_to_s_note(*/light/*)*/);
-	message.push_back(/*pro_to_s_color(*/color/*)*/);
+	message.push_back(pro_to_s_note(light, msg_type));
+	message.push_back(color);
 
 	midiout->sendMessage(&message);
 	message.erase(message.begin(), message.begin()+message.size());
@@ -247,7 +290,7 @@ int main() {
 	std::vector<unsigned char> message_in;
 	int nBytes, color, deltatick;
 	double stamp;
-	LaunchpadPro *lp = new LaunchpadPro();
+	LaunchpadS *lp = new LaunchpadS();
 	int lp_status = lp->connect();
 	if (lp_status < 0) {
 		return 1;
@@ -255,62 +298,8 @@ int main() {
 
 	srand(time(NULL));
 
-	/*
-	// Scroll text
-	message.push_back( 240 );
-	message.push_back( 0 );
-	message.push_back( 32 );
-	message.push_back( 41 );
-	message.push_back( 2 );
-	message.push_back( 16 );
-	message.push_back( 20 ); // Scroll Text
-	message.push_back( 53 ); // Purple
-	message.push_back( 1 ); // Loop Text
-	message.push_back( 5 ); // Speed 5 of 7
-	message.push_back( 'H' );
-	message.push_back( 'e' );
-	message.push_back( 'l' );
-	message.push_back( 'l' );
-	message.push_back( 'o' );
-	message.push_back( ',' );
-	message.push_back( ' ' );
-	message.push_back( 6 ); // Speed 6 of 7
-	message.push_back( 'w' );
-	message.push_back( 'o' );
-	message.push_back( 'r' );
-	message.push_back( 'l' );
-	message.push_back( 'd' );
-	message.push_back( '!' );
-	message.push_back( 247 );
-	midiout->sendMessage( &message );
-	message.erase(message.begin(),  message.begin()+message.size());
-	*/
-
 	// Pulse side LED
-	lp->setPulse(176, LAUNCHPAD_SIDE_LED_ID, 53);
-
-	/*
-	// Program change: 192, 5
-	message.push_back( 192 );
-	message.push_back( 5 );
-	midiout->sendMessage( &message );
-	// Control Change: 176, 7, 100 (volume)
-	message[0] = 176;
-	message[1] = 7;
-	message.push_back( 100 );
-	midiout->sendMessage( &message );
-	// Note On: 144, 64, 90
-	message[0] = 144;
-	message[1] = 64;
-	message[2] = 90;
-	midiout->sendMessage( &message );
-	SLEEP( 500 ); // Platform-dependent ... see example in tests directory.
-	// Note Off: 128, 64, 40
-	message[0] = 128;
-	message[1] = 64;
-	message[2] = 40;
-	midiout->sendMessage( &message );
-	*/
+	//lp->setPulse(176, LAUNCHPAD_PRO_SIDE_LED_ID, 53);
 
 	midifile.read("./Light_Output_Test.mid");
 	midifile.joinTracks();
@@ -334,15 +323,6 @@ int main() {
 			lp->setColor((int)(*mev)[0], note_to_button((int)(*mev)[1]), color);
 
 		}
-
-		// std::cout << dec << mev->tick;
-		// std::cout << '\t' << deltatick;
-		// std::cout << '\t' << mev->track;
-		// std::cout << '\t' << std::hex;
-		// for (int i=0; i < mev->size(); i++) {
-		// 	std::cout << (int)(*mev)[i] << ' ';
-		// }
-		// std::cout << std::endl;
 	}
 
 	std::cout << "Completed MIDI file playback!" << std::endl << std::endl;
