8d27226bf4f0bc4fe88f8bebee27f6244407a3e2
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-07-17 15:08:21 -0700

    Cleanup SplashScreen comments and code

--- src/SplashScreen.cpp
+++ src/SplashScreen.cpp
@@ -12,15 +12,15 @@
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+	#include <wx/wx.h>
 #endif
 
 #ifdef __BORLANDC__
-    #pragma hdrstop
+	#pragma hdrstop
 #endif
 
 #ifdef __WXGTK20__
-    #include <gtk/gtk.h>
+	#include <gtk/gtk.h>
 #endif
 
 #include "wx/dcmemory.h"
@@ -30,13 +30,8 @@
 #include <queue>
 #include <vector>
 
-// ----------------------------------------------------------------------------
-// SplashScreen
-// ----------------------------------------------------------------------------
-
-IMPLEMENT_DYNAMIC_CLASS(SplashScreen, wxFrame)
 BEGIN_EVENT_TABLE(SplashScreen, wxFrame)
-    EVT_CLOSE(SplashScreen::OnCloseWindow)
+	EVT_CLOSE(SplashScreen::OnCloseWindow)
 	#ifdef __WXGTK__
 		EVT_PAINT(SplashScreen::OnPaint)
 	#endif
@@ -102,15 +97,16 @@ static void DrawSplashBitmap(wxDC& dc, const wxBitmap& bitmap, const wxString te
 }
 
 SplashScreen::SplashScreen(wxWindow* parent, wxWindowID id, const wxBitmap& bitmap, std::string text, wxRect textbox, wxFont textfont, wxColor textcolor) : wxFrame() {
-    Create(parent, id, wxEmptyString, wxPoint(0,0), wxSize(100, 100), wxSIMPLE_BORDER | wxSTAY_ON_TOP | wxFRAME_TOOL_WINDOW | wxFRAME_NO_TASKBAR | wxTRANSPARENT_WINDOW);
-    
-    SetBackgroundColour(wxTransparentColor);
-    Refresh();
-    wxEvtHandler::AddFilter(this);
 
-    // splash screen must not be used as parent by the other windows because it
-    // is going to disappear soon, indicate it by giving it this special style
-    SetExtraStyle(GetExtraStyle() | wxWS_EX_TRANSIENT);
+	Create(parent, id, wxEmptyString, wxPoint(0,0), wxSize(100, 100), wxSIMPLE_BORDER | wxSTAY_ON_TOP | wxFRAME_TOOL_WINDOW | wxFRAME_NO_TASKBAR | wxTRANSPARENT_WINDOW);
+	
+	SetBackgroundColour(wxTransparentColor);
+	Refresh();
+	wxEvtHandler::AddFilter(this);
+
+	// splash screen must not be used as parent by the other windows because it
+	// is going to disappear soon, indicate it by giving it this special style
+	SetExtraStyle(GetExtraStyle() | wxWS_EX_TRANSIENT);
 
 	#if defined(__WXGTK20__)
 		gtk_window_set_type_hint(GTK_WINDOW(m_widget), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
@@ -127,49 +123,57 @@ SplashScreen::SplashScreen(wxWindow* parent, wxWindowID id, const wxBitmap& bitm
 		if (bitmap.GetPalette() && !hiColour) SetPalette(* bitmap.GetPalette());
 	#endif
 
-    SetClientSize(bitmap.GetWidth(), bitmap.GetHeight());
-    CentreOnScreen();
-    Show(true);
+	SetClientSize(bitmap.GetWidth(), bitmap.GetHeight());
+	CentreOnScreen();
+	Show(true);
 	Update(); // Without this, you see a blank screen for an instant
 }
 
 SplashScreen::~SplashScreen() {
-    wxEvtHandler::RemoveFilter(this);
-}
-
-int SplashScreen::FilterEvent(wxEvent& event) {
-    const wxEventType t = event.GetEventType();
-    if (t == wxEVT_KEY_DOWN || t == wxEVT_LEFT_DOWN || t == wxEVT_RIGHT_DOWN || t == wxEVT_MIDDLE_DOWN)
-        Close(true);
-
-    return -1;
-}
-
-void SplashScreen::OnNotify(wxTimerEvent& WXUNUSED(event)) {
-    Close(true);
+	wxEvtHandler::RemoveFilter(this);
 }
 
 void SplashScreen::OnCloseWindow(wxCloseEvent& WXUNUSED(event)) {
-    this->Destroy();
+	this->Destroy();
+}
+
+void SplashScreen::OnNotify(wxTimerEvent& WXUNUSED(event)) {
+	Close(true);
 }
 
 void SplashScreen::OnPaint(wxPaintEvent& WXUNUSED(event)) {
-    wxPaintDC dc(this);
+	wxPaintDC dc(this);
 	dc.SetBackgroundMode(wxTRANSPARENT);
 	dc.SetBackground(*wxTRANSPARENT_BRUSH);
-    if (m_bitmap.IsOk()) DrawSplashBitmap(dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
+	if (m_bitmap.IsOk()) DrawSplashBitmap(dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
 }
 
 void SplashScreen::OnEraseBackground(wxEraseEvent& event) {
-    if (event.GetDC() && m_bitmap.IsOk()) {
+	if (event.GetDC() && m_bitmap.IsOk()) {
 		wxDC *dc = event.GetDC();
 		dc->SetBackgroundMode(wxTRANSPARENT);
 		dc->SetBackground(*wxTRANSPARENT_BRUSH);
-        DrawSplashBitmap(*dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
-    } else {
-        wxClientDC dc(this);
+		DrawSplashBitmap(*dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
+	} else {
+		wxClientDC dc(this);
 		dc.SetBackgroundMode(wxTRANSPARENT);
 		dc.SetBackground(*wxTRANSPARENT_BRUSH);
-        if (m_bitmap.IsOk()) DrawSplashBitmap(dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
-    }
+		if (m_bitmap.IsOk()) DrawSplashBitmap(dc, m_bitmap, m_text, m_textbox, m_textfont, m_textcolor);
+	}
+}
+
+void SplashScreen::SetBitmap(const wxBitmap& bitmap) {
+	m_bitmap = bitmap;
+}
+
+wxBitmap& SplashScreen::GetBitmap() {
+	return m_bitmap;
+}
+
+int SplashScreen::FilterEvent(wxEvent& event) {
+	const wxEventType t = event.GetEventType();
+	if (t == wxEVT_KEY_DOWN || t == wxEVT_LEFT_DOWN || t == wxEVT_RIGHT_DOWN || t == wxEVT_MIDDLE_DOWN)
+		Close(true);
+	
+	return -1;
 }
--- src/SplashScreen.h
+++ src/SplashScreen.h
@@ -12,7 +12,7 @@
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+	#include <wx/wx.h>
 #endif
 
 #include "wx/bitmap.h"
@@ -20,36 +20,29 @@
 #include "wx/frame.h"
 #include "wx/timer.h"
 
-/*
- * SplashScreen
- */
-
 class SplashScreen: public wxFrame, public wxEventFilter {
-    public:
         SplashScreen() { Init(); }
-        SplashScreen(wxWindow* parent, wxWindowID id, const wxBitmap& bitmap, std::string text, wxRect textbox, wxFont textfont, wxColor textcolor);
-        virtual ~SplashScreen();
+	public:
+		SplashScreen();
+		virtual ~SplashScreen();
 
-        void OnCloseWindow(wxCloseEvent& event);
-        void OnNotify(wxTimerEvent& event);
-        
-        void OnPaint(wxPaintEvent& event);
-        void OnEraseBackground(wxEraseEvent& event);
-        
-        void SetBitmap(const wxBitmap& bitmap) { m_bitmap = bitmap; }
-        wxBitmap& GetBitmap() { return m_bitmap; }
+		void OnCloseWindow(wxCloseEvent& event);
+		void OnNotify(wxTimerEvent& event);
+		void OnPaint(wxPaintEvent& event);
+		void OnEraseBackground(wxEraseEvent& event);
+		
+		void SetBitmap(const wxBitmap& bitmap);
+		wxBitmap& GetBitmap();
 
-        // Override wxEventFilter method to hide splash screen on any user input.
-        virtual int FilterEvent(wxEvent& event);
+		virtual int FilterEvent(wxEvent& event);
 
-    protected:
-        wxBitmap m_bitmap;
+	protected:
+		wxBitmap m_bitmap;
 		wxString m_text;
 		wxRect m_textbox;
 		wxFont m_textfont;
 		wxColor m_textcolor;
 
-        DECLARE_DYNAMIC_CLASS(SplashScreen)
-        DECLARE_EVENT_TABLE()
-        wxDECLARE_NO_COPY_CLASS(SplashScreen);
+		DECLARE_EVENT_TABLE()
+		wxDECLARE_NO_COPY_CLASS(SplashScreen);
 };
