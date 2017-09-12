27ba1024a4500089105244eda15f30abfa1deacb
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-09-11 20:26:57 -0700

    Add missing FindNightwaveCore and FindHOWL (whoops!)

---
+++ CMakeModules/FindHOWL.cmake
@@ -0,0 +1,70 @@
+# -*-cmake-*-
+# 
+# Nightwave Studios - FindHOWL.cmake
+# https://www.nightwave.co
+# 
+# Search for HOWL library files
+# Based off of https://github.com/buaazp/zimg/blob/master/src/CMakeModules/FindGraphicsMagick.cmake
+#
+# Once loaded this will define:
+#   HOWL_FOUND       - system has HOWL
+#   HOWL_INCLUDE_DIR - include directory for HOWL
+#   HOWL_LIBRARY_DIR - library directory for HOWL
+#   HOWL_LIBRARIES   - libraries you need to link to
+#
+ 
+SET(HOWL_FOUND "NO")
+ 
+FIND_PATH(HOWL_INCLUDE_DIR
+	NAMES "HOWL/Helpers.h"
+	PATHS
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\HOWL\\Current;BinPath]"
+	"$ENV{HOWL_LOCATION}"
+	"$ENV{HOWL_LOCATION}/include"
+	"$ENV{HOWL_LOCATION}/include/HOWL"
+	"$ENV{HOWL_HOME}/include"
+	/usr/include/
+	/usr/include/HOWL
+	/usr/local/include
+	/usr/local/include/HOWL
+	/opt/local/include/HOWL
+	)
+
+FIND_LIBRARY(HOWL
+	NAMES libHOWL HOWL
+	PATHS 
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\HOWL\\Current;BinPath]"
+	"$ENV{HOWL_LOCATION}/HOWL/.libs"
+	"$ENV{HOWL_LOCATION}/lib"
+	"$ENV{HOWL_HOME}/lib"
+	/usr/lib64
+	/usr/local/lib64
+	/opt/local/lib64
+	/usr/lib
+	/usr/local/lib
+	/opt/local/lib
+	DOC "HOWL library"
+	)
+
+ 
+SET(HOWL_LIBRARIES ${HOWL})
+ 
+IF (HOWL_INCLUDE_DIR)
+	IF(HOWL_LIBRARIES)
+		SET(HOWL_FOUND "YES")
+		GET_FILENAME_COMPONENT(HOWL_LIBRARY_DIR ${HOWL} PATH)
+	ENDIF(HOWL_LIBRARIES)
+ENDIF(HOWL_INCLUDE_DIR)
+ 
+IF(NOT HOWL_FOUND)
+	# make FIND_PACKAGE friendly
+	IF(NOT HOWL_FIND_QUIETLY)
+		IF(HOWL_FIND_REQUIRED)
+			MESSAGE(FATAL_ERROR "HOWL required, please specify it's location with HOWL_HOME or HOWL_LOCATION")
+		ELSE(HOWL_FIND_REQUIRED)
+			MESSAGE(STATUS "HOWL was not found.")
+		ENDIF(HOWL_FIND_REQUIRED)
+	ENDIF(NOT HOWL_FIND_QUIETLY)
+ENDIF(NOT HOWL_FOUND)
+ 
+#####
---
+++ CMakeModules/FindNightwaveCore.cmake
@@ -0,0 +1,71 @@
+# -*-cmake-*-
+# 
+# Nightwave Studios - FindNightwaveCore.cmake
+# https://www.nightwave.co
+# 
+# Search for NightwaveCore library files
+# Based off of https://github.com/buaazp/zimg/blob/master/src/CMakeModules/FindGraphicsMagick.cmake
+#
+# Once loaded this will define:
+#   NIGHTWAVE_CORE_FOUND       - system has NightwaveCore
+#   NIGHTWAVE_CORE_INCLUDE_DIR - include directory for NightwaveCore
+#   NIGHTWAVE_CORE_LIBRARY_DIR - library directory for NightwaveCore
+#   NIGHTWAVE_CORE_LIBRARIES   - libraries you need to link to
+#
+ 
+SET(NIGHTWAVE_CORE_FOUND "NO")
+ 
+FIND_PATH(NIGHTWAVE_CORE_INCLUDE_DIR
+	NAMES "NightwaveCore/Helpers.h"
+	PATHS
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\NightwaveCore\\Current;BinPath]"
+	"$ENV{NIGHTWAVE_CORE_LOCATION}"
+	"$ENV{NIGHTWAVE_CORE_LOCATION}/include"
+	"$ENV{NIGHTWAVE_CORE_LOCATION}/include/NightwaveCore"
+	"$ENV{NIGHTWAVE_CORE_HOME}/include"
+	/usr/include/
+	/usr/include/NightwaveCore
+	/usr/local/include
+	/usr/local/include/NightwaveCore
+	/opt/local/include/NightwaveCore
+	)
+
+FIND_LIBRARY(NightwaveCore
+	NAMES libNightwaveCore NightwaveCore
+	PATHS 
+	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\NightwaveCore\\Current;BinPath]"
+	"$ENV{NIGHTWAVE_CORE_LOCATION}/NIGHTWAVE_CORE/.libs"
+	"$ENV{NIGHTWAVE_CORE_LOCATION}/lib"
+	"$ENV{NIGHTWAVE_CORE_HOME}/lib"
+	/usr/lib64
+	/usr/local/lib64
+	/opt/local/lib64
+	/usr/lib
+	/usr/local/lib
+	/opt/local/lib
+	DOC "NightwaveCore library"
+	)
+
+ 
+SET(NIGHTWAVE_CORE_LIBRARIES ${NightwaveCore})
+ 
+IF (NIGHTWAVE_CORE_INCLUDE_DIR)
+	IF(NIGHTWAVE_CORE_LIBRARIES)
+		SET(NIGHTWAVE_CORE_FOUND "YES")
+		GET_FILENAME_COMPONENT(NIGHTWAVE_CORE_LIBRARY_DIR ${NightwaveCore} PATH)
+	ENDIF(NIGHTWAVE_CORE_LIBRARIES)
+ENDIF(NIGHTWAVE_CORE_INCLUDE_DIR)
+ 
+IF(NOT NIGHTWAVE_CORE_FOUND)
+	# make FIND_PACKAGE friendly
+	IF(NOT NIGHTWAVE_CORE_FIND_QUIETLY)
+		IF(NIGHTWAVE_CORE_FIND_REQUIRED)
+			MESSAGE(FATAL_ERROR
+				"NightwaveCore required, please specify it's location with NIGHTWAVE_CORE_HOME or NIGHTWAVE_CORE_LOCATION")
+		ELSE(NIGHTWAVE_CORE_FIND_REQUIRED)
+			MESSAGE(STATUS "NightwaveCore was not found.")
+		ENDIF(NIGHTWAVE_CORE_FIND_REQUIRED)
+	ENDIF(NOT NIGHTWAVE_CORE_FIND_QUIETLY)
+ENDIF(NOT NIGHTWAVE_CORE_FOUND)
+ 
+#####
