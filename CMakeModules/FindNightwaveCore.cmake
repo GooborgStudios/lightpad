c2f0f99a5cebc1615ccfad495dc0230e874cdd34
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2017-12-21 01:45:12 -0800

    Update CMake Find modules

--- CMakeModules/FindNightwaveCore.cmake
+++ CMakeModules/FindNightwaveCore.cmake
@@ -1,28 +1,27 @@
 # -*-cmake-*-
-# 
+#
 # Nightwave Studios - FindNightwaveCore.cmake
 # https://www.nightwave.co
-# 
+#
 # Search for NightwaveCore library files
-# Based off of https://github.com/buaazp/zimg/blob/master/src/CMakeModules/FindGraphicsMagick.cmake
 #
 # Once loaded this will define:
-#   NIGHTWAVE_CORE_FOUND       - system has NightwaveCore
-#   NIGHTWAVE_CORE_INCLUDE_DIR - include directory for NightwaveCore
-#   NIGHTWAVE_CORE_LIBRARY_DIR - library directory for NightwaveCore
-#   NIGHTWAVE_CORE_LIBRARIES   - libraries you need to link to
-#
+#   NightwaveCore_FOUND       - system has NightwaveCore
+#   NightwaveCore_INCLUDE_DIR - include directory for NightwaveCore
+#   NightwaveCore_LIBRARY_DIR - library directory for NightwaveCore
+#   NightwaveCore_LIBRARIES   - libraries you need to link to
 
-SET(NIGHTWAVE_CORE_FOUND "NO")
 
-FIND_PATH(NIGHTWAVE_CORE_INCLUDE_DIR
+
+# Find headers
+find_path(NightwaveCore_INCLUDE_DIR
 	NAMES "NightwaveCore/NightwaveCore.h"
 	PATHS
 	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\NightwaveCore\\Current;BinPath]"
-	"$ENV{NIGHTWAVE_CORE_LOCATION}"
-	"$ENV{NIGHTWAVE_CORE_LOCATION}/include"
-	"$ENV{NIGHTWAVE_CORE_LOCATION}/include/NightwaveCore"
-	"$ENV{NIGHTWAVE_CORE_HOME}/include"
+	"$ENV{NightwaveCore_LOCATION}"
+	"$ENV{NightwaveCore_LOCATION}/include"
+	"$ENV{NightwaveCore_LOCATION}/include/NightwaveCore"
+	"$ENV{NightwaveCore_HOME}/include"
 	/usr/include/
 	/usr/include/NightwaveCore
 	/usr/local/include
@@ -30,13 +29,14 @@ FIND_PATH(NIGHTWAVE_CORE_INCLUDE_DIR
 	/opt/local/include/NightwaveCore
 )
 
-FIND_LIBRARY(NightwaveCore
+# Find compiled library file
+find_library(NightwaveCore_LIBRARIES
 	NAMES libNightwaveCore NightwaveCore
 	PATHS 
 	"[HKEY_LOCAL_MACHINE\\SOFTWARE\\NightwaveCore\\Current;BinPath]"
-	"$ENV{NIGHTWAVE_CORE_LOCATION}/NIGHTWAVE_CORE/.libs"
-	"$ENV{NIGHTWAVE_CORE_LOCATION}/lib"
-	"$ENV{NIGHTWAVE_CORE_HOME}/lib"
+	"$ENV{NightwaveCore_LOCATION}/NightwaveCore/.libs"
+	"$ENV{NightwaveCore_LOCATION}/lib"
+	"$ENV{NightwaveCore_HOME}/lib"
 	/usr/lib64
 	/usr/local/lib64
 	/opt/local/lib64
@@ -46,24 +46,18 @@ FIND_LIBRARY(NightwaveCore
 	DOC "NightwaveCore library"
 )
 
-SET(NIGHTWAVE_CORE_LIBRARIES ${NightwaveCore})
+# Get path to library
+get_filename_component(NightwaveCore_LIBRARY_DIR ${NightwaveCore_LIBRARIES} PATH)
 
-IF(NIGHTWAVE_CORE_INCLUDE_DIR)
-	IF(NIGHTWAVE_CORE_LIBRARIES)
-		SET(NIGHTWAVE_CORE_FOUND "YES")
-		GET_FILENAME_COMPONENT(NIGHTWAVE_CORE_LIBRARY_DIR ${NightwaveCore} PATH)
-	ENDIF(NIGHTWAVE_CORE_LIBRARIES)
-ENDIF(NIGHTWAVE_CORE_INCLUDE_DIR)
+# Set variables as advanced (hide from GUI unless "show advanced checked")
+mark_as_advanced(NightwaveCore_LIBRARIES NightwaveCore_LIBRARY_DIR NightwaveCore_INCLUDE_DIR)
 
-IF(NOT NIGHTWAVE_CORE_FOUND)
-	# make FIND_PACKAGE friendly
-	IF(NOT NIGHTWAVE_CORE_FIND_QUIETLY)
-		IF(NIGHTWAVE_CORE_FIND_REQUIRED)
-			MESSAGE(FATAL_ERROR "NightwaveCore required, please specify it's location with NIGHTWAVE_CORE_HOME or NIGHTWAVE_CORE_LOCATION")
-		ELSE(NIGHTWAVE_CORE_FIND_REQUIRED)
-			MESSAGE(STATUS "NightwaveCore was not found.")
-		ENDIF(NIGHTWAVE_CORE_FIND_REQUIRED)
-	ENDIF(NOT NIGHTWAVE_CORE_FIND_QUIETLY)
-ENDIF(NOT NIGHTWAVE_CORE_FOUND)
- 
-#####
+# Set NightwaveCore_FOUND and print message
+INCLUDE(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+	NightwaveCore
+	DEFAULT_MSG
+
+	NightwaveCore_LIBRARIES
+	NightwaveCore_INCLUDE_DIR
+)
